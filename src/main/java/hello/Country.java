ceac9aebd0044a9c1f7fa8ad7235ba8abd9c9e35
VinodhThiagarajan86 <vinodh@vinodhthiagarajan.info>
2014-10-14 22:24:26 -0500

    SOAPService
    SOAP Service Using Spring Boot

---
+++ src/main/java/hello/Country.java
@@ -0,0 +1,34 @@
+/*package hello;
+
+public class Country {
+
+	String name = "";
+	String capital = "";
+	String currency = "";
+	long population= 0;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCapital() {
+		return capital;
+	}
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public long getPopulation() {
+		return population;
+	}
+	public void setPopulation(long population) {
+		this.population = population;
+	}
+}
+*/
